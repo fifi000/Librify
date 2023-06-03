12ec321e4fd47d77100d01da688ff57fc61737db
Filip Kociok <filipkociok@gmail.com>
2023-06-03 10:12:38 +0200

    Adds a Mapper<QString, Status>  which is string to enum

--- Controllers/enums.h
+++ Controllers/enums.h
@@ -1,11 +1,16 @@
 #ifndef ENUMS_H
 #define ENUMS_H
 
+#include <QHash>
+#include <QString>
+
 enum Status
 {
     Reading,
     ToRead,
     Finished
 };
+
+const QHash<QString, Status> StatusConventer{{"Reading", Reading}, {"To Read", ToRead}, {"Finished", Finished}};
 
 #endif // ENUMS_H
